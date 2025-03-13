a96913c53ae25717acdaad0be7ffbe6a1d2a96c4
dcakinn <88024837+dcakinn@users.noreply.github.com>
2025-03-13 23:35:33 +0300

    Update index.js
    comment lines added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // Tailwind veya genel stiller için
 
+// uygulamayı başlatır ve tüm bileşenleri DOM'a aktarır. 
+// React.StrictMode kullanılarak geliştirici hataları ve uyarıları denetlenir.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

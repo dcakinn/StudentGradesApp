bf230f1db3419e678a0ae1fe622219b87f98592c
dcakinn <88024837+dcakinn@users.noreply.github.com>
2025-03-13 23:32:41 +0300

    Update App.js
    comment lines added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { GradeProvider } from "./context/GradesContext";
 import GradeEntry from "./components/GradeEntry";
 import StudentGrades from "./components/StudentGrades";
 
+// React Router ve Context API kullanılarak öğrenci not giriş ve görüntüleme uygulaması oluşturulmuştur.
+// sayfa değişikliklerini yönetmek için BrowserRouter kullanılmıştır. sayfa refresh olmadan bileşen değişimi sağlanır.
 const App = () => {
   return (
     <GradeProvider>
